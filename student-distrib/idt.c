058a9e230296b6be2ff8b35c4487d6d207da60eb
Tim Hostetler <tdh911@gmail.com>
2017-03-11 14:56:00 -0600

    added system exception handlers

--- student-distrib/idt.c
+++ student-distrib/idt.c
@@ -1,9 +1,28 @@
 #include "idt.h"
 
-void exception_handler(){
-    printf("error!");
-    while(1);
-}
+//static array of all system handlers
+static uint32_t sys_handlers[NUM_SYS_HANDLERS] = {
+    (uint32_t)divide_handler,
+    (uint32_t)debug_handler,
+    (uint32_t)nmi_interrupt_handler,
+    (uint32_t)breakpoint_handler,
+    (uint32_t)overflow_handler,
+    (uint32_t)bound_range_handler,
+    (uint32_t)invalid_opcode_handler,
+    (uint32_t)device_not_avail_handler,
+    (uint32_t)dbl_fault_handler,
+    (uint32_t)coprocess_seg_handler,
+    (uint32_t)inval_tss_handler,
+    (uint32_t)seg_not_pres_handler,
+    (uint32_t)stack_fault_handler,
+    (uint32_t)gen_protect_handler,
+    (uint32_t)page_fault_handler,
+    (uint32_t)exception_handler,//general exception here
+    (uint32_t)float_point_handler,
+    (uint32_t)align_check_handler,
+    (uint32_t)machine_check_handler,
+    (uint32_t)simd_float_point_handler
+};
 
 /* init_idt
  *
@@ -25,12 +44,12 @@ void init_idt(){
 
     idt[i].seg_selector = KERNEL_CS;
     //priority level should be 0 for non system calls
-    idt[i].dpl = 0;
+    idt[i].dpl = RING0;
 
     idt[i].size = 1;
     idt[i].present = 1;
 
-    //mark every descriptor as 01100 in bit values
+    //mark every descriptor as 01110 in bit values to signify interrupt gate
     idt[i].reserved0 = 0;
     idt[i].reserved1 = 1;
     idt[i].reserved2 = 1;
@@ -38,10 +57,155 @@ void init_idt(){
     idt[i].reserved4 = 0;
 
     //if system call, priority level is 3
-    if(i == 0x80)
-      idt[i].dpl = 3;
+    if(i == SYS_CALL)
+      idt[i].dpl = RING3;
 
-      SET_IDT_ENTRY(idt[i], exception_handler);
+    //assign correct handler
+    if(i < NUM_SYS_HANDLERS)
+        SET_IDT_ENTRY(idt[i],sys_handlers[i]);
+    else
+        SET_IDT_ENTRY(idt[i],exception_handler);
+
   }
 
+}
+
+/* exception_handler
+ *
+ * DESCRIPTION: General exception handler
+ * INPUT/OUTPUT: none
+ * SIDE EFFECTS: none
+ */
+void exception_handler(){
+    clear();
+    printf("Unknown error occured");
+    while(1);
+}
+
+/* divide_handler
+ *
+ * DESCRIPTION: Handler for Divide Error Exception (#DE)
+                Called upon a divide by 0 or result would overflow
+ * INPUT/OUTPUT: none
+ * SIDE EFFECTS: none
+ */
+void divide_handler(){
+    clear();
+    printf("Interrupt 0 - Divide Error Exception\n");
+    while(1);
+}
+
+/* debug_handler
+ *
+ * DESCRIPTION: Handler for Divide Error Exception (#DE)
+                Called upon a divide by 0 or result would overflow
+ * INPUT/OUTPUT: none
+ * SIDE EFFECTS: none
+ */
+void debug_handler(){
+    clear();
+    printf("Interrupt 1 - Debug Exception\n");
+    while(1);
+}
+
+void nmi_interrupt_handler(){
+    clear();
+    printf("Interrupt 2 - Nonmaskable Interrupt\n");
+    while(1);
+}
+
+void breakpoint_handler(){
+    clear();
+    printf("Interrupt 3 - Breakpoint Exception\n");
+    while(1);
+}
+
+void overflow_handler(){
+    clear();
+    printf("Interrupt 4 - Overflow Exception\n");
+    while(1);
+}
+
+void bound_range_handler(){
+    clear();
+    printf("Interrupt 5 - BOUND Range Exceeded\n");
+    while(1);
+}
+
+void invalid_opcode_handler(){
+    clear();
+    printf("Interrupt 6 - Invalid Opcode\n");
+    while(1);
+}
+
+void device_not_avail_handler(){
+    clear();
+    printf("Interrupt 7 - Device Not Available\n");
+    while(1);
+}
+
+void dbl_fault_handler(){
+    clear();
+    printf("Interrupt 8 - Double Fault\n");
+    while(1);
+}
+
+void coprocess_seg_handler(){
+    clear();
+    printf("Interrupt 9 - Coprocessor Segment Overrun\n");
+    while(1);
+}
+
+void inval_tss_handler(){
+    clear();
+    printf("Interrupt 10 - Invalid TSS\n");
+    while(1);
+}
+
+void seg_not_pres_handler(){
+    clear();
+    printf("Interrupt 11 - Segment Not Present\n");
+    while(1);
+}
+
+void stack_fault_handler(){
+    clear();
+    printf("Interrupt 12 - Stack Fault Exception\n");
+    while(1);
+}
+
+void gen_protect_handler(){
+    clear();
+    printf("Interrupt 13 - General Protection Exception\n");
+    while(1);
+}
+
+void page_fault_handler(){
+    clear();
+    printf("Interrupt 14 - Page-Fault Exception\n");
+    while(1);
+}
+
+void float_point_handler(){
+    clear();
+    printf("Interrupt 16 - x87 FPU Floating-Point Error\n");
+    while(1);
+}
+
+void align_check_handler(){
+    clear();
+    printf("Interrupt 17 - Alignment Check Exception\n");
+    while(1);
+}
+
+void machine_check_handler(){
+    clear();
+    printf("Interrupt 18 - Machine-Check Exception\n");
+    while(1);
+}
+
+void simd_float_point_handler(){
+    clear();
+    printf("Interrupt 19 - SIMD Floating-Point Exception\n");
+    while(1);
 }
--- student-distrib/idt.h
+++ student-distrib/idt.h
@@ -1,7 +1,52 @@
 #include "x86_desc.h"
 #include "lib.h"
 
+#define SYS_CALL 0x80
+#define NUM_SYS_HANDLERS 20
+
+#define RING0 0
+#define RING1 1
+#define RING2 2
+#define RING3 3
 
 extern void init_idt();
 
 void exception_handler();
+
+void divide_handler();
+
+void debug_handler();
+
+void nmi_interrupt_handler();
+
+void breakpoint_handler();
+
+void overflow_handler();
+
+void bound_range_handler();
+
+void invalid_opcode_handler();
+
+void device_not_avail_handler();
+
+void dbl_fault_handler();
+
+void coprocess_seg_handler();
+
+void inval_tss_handler();
+
+void seg_not_pres_handler();
+
+void stack_fault_handler();
+
+void gen_protect_handler();
+
+void page_fault_handler();
+
+void float_point_handler();
+
+void align_check_handler();
+
+void machine_check_handler();
+
+void simd_float_point_handler();
