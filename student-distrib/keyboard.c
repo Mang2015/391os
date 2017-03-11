ecc1bebfda950421e51a4451fad1f91b8ea839d0
pitta2 <pitta2@illinois.edu>
2017-03-11 17:06:25 -0600

    finished writing keyboard initialization, currently working on writing the keyboard handler

--- student-distrib/keyboard.c
+++ student-distrib/keyboard.c
@@ -1,1 +1,28 @@
 // this is the implementation file for keyboard.h
+
+#include "keyboard.h"
+
+void keyboard_init(void)
+{
+    /*
+        1) we need to read from the keyboard port (x60)
+        2) we need to enable the keyboard port on the PIC
+    */
+
+    uint8_t keyboard_read;
+
+    keyboard_read = inb(KEYBOARD_BUFFER_PORT);
+    enable_irq(KEYBOARD_IRQ_NUM);
+}
+
+void keyboard_handler()
+{
+    /*
+        1) create the mapping mechanism
+        2) call end of interrupt signal
+    */
+
+    // perform mapping mechanism
+
+    send_eoi(KEYBOARD_IRQ_NUM);
+}
--- student-distrib/keyboard.h
+++ student-distrib/keyboard.h
@@ -1,1 +1,13 @@
 // this is the header file for initializing the keyboard
+
+#include "x86_desc.h"
+#include "lib.h"
+#include "i8259.h"
+
+#define KEYBOARD_BUFFER_PORT 0x60
+#define KEYBOARD_STATUS_PORT 0x64
+#define KEYBOARD_IRQ_NUM 1
+
+extern void keyboard_init(void);
+
+extern void keyboard_handler();
