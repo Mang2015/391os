037f83c1a56fccbd1150e973a7bc2a4f9f5d4cf1
Umang <umangchavan20@gmail.com>
2017-03-11 16:47:17 -0600

    rtc files added, currently still working on them

---
+++ student-distrib/rtc.c
@@ -0,0 +1,17 @@
+#include "rtc.h"
+
+unsigned char cur_val;
+
+void rtc_init(void) {
+
+  outb(STAT_REG_B, RTC_PORT);             // Select status register B and disable NMI
+  cur_val = inb(RW_CMOS);                 // load cur_val with value in status register B
+  outb(STAT_REG_B, RTC_PORT);             // reset the index back into RTC_PORT
+  outb(cur_val | ENABLE_BIT_SIX, RW_CMOS);  // "OR" current value with 0x40 and write into CMOS port
+
+
+  enable_irq(RTC_IRQ_NUM);                // enable on PIC
+
+  /* need to figure out how to change rate, am working on that */
+
+}
---
+++ student-distrib/rtc.h
@@ -0,0 +1,14 @@
+#include "lib.h"
+#include "types.h"
+#include "i8259.h"
+
+
+#define RTC_PORT  0x70
+#define STAT_REG_A  0x8A
+#define STAT_REG_B  0x8B
+#define ENABLE_BIT_SIX  0x40
+#define RTC_IRQ_NUM 8
+#define RW_CMOS 0x71
+
+void rtc_init(void);
+void rtc_handler()
