652d8c4b6f32d3aa0e51ce970989bb4433618867
Rohit Thotakaura <rthotakura97@gmail.com>
2017-03-11 12:54:40 -0600

    commented init idt, still need to write exception handleers and fill table correctly

--- student-distrib/idt.c
+++ student-distrib/idt.c
@@ -5,26 +5,39 @@ void exception_handler(){
     while(1);
 }
 
+/* init_idt
+ *
+ * DESCRIPTION: Initializes IDT by looping throught table and setting
+ *              corresponding descriptor bits. Also sets offsets to
+ *              for various entries in the table.
+ * INPUT/OUTPUT: none
+ * SIDE EFFECTS: fills IDT with corresponding descriptor values and offsets
+ */
 void init_idt(){
 
+  //load the pointer to idt into the IDTR
   lidt(idt_desc_ptr);
 
   int i;
+  //loop through each vector in the idt and set the corresponding
+  //bits in the descriptor
   for(i=0;i<NUM_VEC;i++){
+
     idt[i].seg_selector = KERNEL_CS;
+    //priority level should be 0 for non system calls
     idt[i].dpl = 0;
+
     idt[i].size = 1;
     idt[i].present = 1;
+
+    //mark every descriptor as 01100 in bit values
     idt[i].reserved0 = 0;
     idt[i].reserved1 = 1;
     idt[i].reserved2 = 1;
-    idt[i].reserved3 = 1;
+    idt[i].reserved3 = 0;
     idt[i].reserved4 = 0;
 
-    if(i>=32){
-      idt[i].reserved3 = 0;
-    }
-
+    //if system call, priority level is 3
     if(i == 0x80)
       idt[i].dpl = 3;
 
