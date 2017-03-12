c34d4de2c29f438f6f011e8e63678c133bfd1a3c
Pitta <pitta2@illinois.edu>
2017-03-12 18:05:56 -0500

    created temporary header file for paging, this is something distributed alb said can be helpful

--- student-distrib/paging.h
+++ student-distrib/paging.h
@@ -1,1 +1,12 @@
 // this is going to be the paging.h file
+#ifndef PAGING_H
+#define PAGING_H
+
+#include "x86_desc.h"
+#include "lib.h"
+#include "i8259"
+
+int page_directory __attribute__((aligned (number_of_bytes)));
+int page_table __attribute__((aligned (number_of_bytes)));
+
+#endif
