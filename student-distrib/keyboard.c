75bdc056f8550e02d0645aad9d458a42458c2f7a
pitta2 <pitta2@illinois.edu>
2017-03-11 20:46:06 -0600

    deleted unnecessary code in initialization of keyboard

--- student-distrib/keyboard.c
+++ student-distrib/keyboard.c
@@ -23,16 +23,9 @@ static uint8_t ascii_val[36] = {
     '4', '5', '6', '7', '8', '9'
 
 };
+
 void keyboard_init(void)
 {
-    /*
-        1) we need to read from the keyboard port (x60)
-        2) we need to enable the keyboard port on the PIC
-    */
-
-    //uint8_t keyboard_read;
-
-    //keyboard_read = inb(KEYBOARD_BUFFER_PORT);
     enable_irq(KEYBOARD_IRQ_NUM);
 }
 
