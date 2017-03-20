50ac3341f67bcc9599138c4c2bd241f2d1f70e39
Tim Hostetler <tdh911@gmail.com>
2017-03-20 16:24:12 -0400

    changed argument passing to sys_hand function

--- student-distrib/sys_handlers.c
+++ student-distrib/sys_handlers.c
@@ -1,15 +1,17 @@
 #include "sys_handlers.h"
 
-int32_t halt();
-int32_t execute();
-int32_t read();
-int32_t write();
-int32_t open();
-int32_t close();
-int32_t getargs();
-int32_t vidmap();
-int32_t set_handler();
-int32_t sigreturn();
+int32_t halt(uint8_t status);
+int32_t execute(const uint8_t* command);
+int32_t read(int32_t fd, void* buf, int32_t nbytes);
+int32_t write(int32_t fd, const void* buf, int32_t nbytes);
+int32_t open(const uint8_t* filename);
+int32_t close(int32_t fd);
+int32_t getargs(uint8_t* buf, int32_t nbytes);
+int32_t vidmap(uint8_t** screen_start);
+int32_t set_handler(int32_t signum, void* handler_address);
+int32_t sigreturn(void);
+
+
 
 
 /* system_handler
@@ -19,105 +21,70 @@ int32_t sigreturn();
  * SIDE EFFECTS: none
  */
 void system_handler(){
-    int32_t instr;
+    uint32_t instr,arg0,arg1,arg2;
     asm volatile("movl %%eax,%0":"=r"(instr));
+    asm volatile("movl %%ebx,%0":"=r"(arg0));
+    asm volatile("movl %%ecx,%0":"=r"(arg1));
+    asm volatile("movl %%edx,%0":"=r"(arg2));
 
     if(instr == SYS_HALT){
-        halt();
+        uint8_t stat = 0xFF & arg0;
+        halt(stat);
     }
     else if(instr == SYS_EXECUTE){
-        execute();
+        execute((const uint8_t*)arg0);
     }
     else if(instr == SYS_READ){
-        read();
+        read((int32_t)arg0,(void*)arg1,(int32_t)arg2);
     }
     else if(instr == SYS_WRITE){
-        write();
+        write((int32_t)arg0,(const void*)arg1,(int32_t)arg2);
     }
     else if(instr == SYS_OPEN){
-        open();
+        open((const uint8_t*)arg0);
     }
     else if(instr == SYS_CLOSE){
-        close();
+        close((int32_t)arg0);
     }
     else if(instr == SYS_GETARGS){
-        getargs();
+        getargs((uint8_t*)arg0,(int32_t)arg1);
     }
     else if(instr == SYS_VIDMAP){
-        vidmap();
+        vidmap((uint8_t**)arg0);
     }
     else if(instr == SYS_SET_HANDLER){
-        set_handler();
+        set_handler((int32_t)arg0,(void*)handler_address);
     }
     else if(instr == SYS_SIGRETURN){
         sigreturn();
     }
-
-
-
 }
 
-int32_t halt(){
-    uint8_t status;
-    asm volatile("movb %%bl,%0":"=r"(status));
+int32_t halt(uint8_t status){
 }
 
-int32_t execute(){
-    uint8_t* command;
-    asm volatile("movl %%ebx,%0":"=r"(command));
+int32_t execute(const uint8_t* command){
 }
 
-int32_t read(){
-    int32_t fd;
-    void* buf;
-    int32_t nbytes;
-
-    asm volatile("movl %%ebx,%0":"=r"(fd));
-    asm volatile("movl %%ecx,%0":"=r"(buf));
-    asm volatile("movl %%edx,%0":"=r"(nbytes));
+int32_t read(int32_t fd, void* buf, int32_t nbytes){
 }
 
-int32_t write(){
-    int32_t fd;
-    void* buf;//constant
-    int32_t nybtes;
-
-    asm volatile("movl %%ebx,%0":"=r"(fd));
-    asm volatile("movl %%ecx,%0":"=r"(buf));
-    asm volatile("movl %%edx,%0":"=r"(nbytes));
+int32_t write(int32_t fd, const void* buf, int32_t nbytes){
 }
 
-int32_t open(){
-    uint8_t* filename;
-
-    asm volatile("movl %%ebx,%0":"=r"(filename));
+int32_t open(const uint8_t* filename){
 }
 
-int32_t close(){
-    int32_t fd;
-
-    asm volatile("movl %%ebx,%0":"=r"(fd));
-
+int32_t close(int32_t fd){
 }
 
-int32_t getargs(){
-    uint8_t *buf;
-    int32_t nbytes;
-    asm volatile("movl %%ebx,%0":"=r"(buf));
-    asm volatile("movl %%ecx,%0":"=r"(nbytes));
+int32_t getargs(uint8_t* buf, int32_t nbytes){
 }
 
-int32_t vidmap(){
-    uint8_t **screen_start;
-
-    asm volatile("movl %%ebx,%0":"=r"(screen_start));
+int32_t vidmap(uint8_t** screen_start){
 }
 
-int32_t set_handler(){
-    int32_t signum;
-    void* handler_address;
-    asm volatile("movl %%ebx,%0":"=r"(signum));
-    asm volatile("movl %%ecx,%0":"=r"(handler_address));
+int32_t set_handler(int32_t signum, void* handler_address){
 }
 
 int32_t sigreturn(){
