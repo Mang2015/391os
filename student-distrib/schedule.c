0881dafb54b44bf2d963691c20d1734cade41223
vvpitta <vvpitta@gmail.com>
2017-04-27 13:52:03 -0500

    added scheduler files, need to start working on them

---
+++ student-distrib/schedule.c
@@ -0,0 +1,7 @@
+//this is for code for scheduler
+#include "schedule.h"
+
+void schedule_init(void)
+{
+    //do something here
+}
---
+++ student-distrib/schedule.h
@@ -0,0 +1,7 @@
+//this file is for scheduling heaader file
+#ifndef SCHEDULE_H
+#define SCHEDULE_H
+
+extern void schedule_init(void);
+
+#endif
